b79e371c745f758818d5527a3876f1c26381dea8
Syed Suhaan <154574962+Syed-Suhaan@users.noreply.github.com>
2026-02-08 15:25:46 +0530

    Add plot4.R to visualize power consumption data
    This script processes household power consumption data, subsets it for specific dates, and generates a multi-plot PNG file displaying Global Active Power, Voltage, Energy sub metering, and Global Reactive Power over time.

---
+++ plot4.R
@@ -0,0 +1,68 @@
+rm(list = ls())
+
+data <- read.table("household_power_consumption.txt",
+                   header = TRUE,
+                   sep = ";",
+                   na.strings = "?")
+
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time),
+                           format = "%d/%m/%Y %H:%M:%S")
+
+num_cols <- c("Global_active_power",
+              "Voltage",
+              "Sub_metering_1",
+              "Sub_metering_2",
+              "Sub_metering_3",
+              "Global_reactive_power")
+
+data[num_cols] <- lapply(data[num_cols], as.numeric)
+
+subset_data <- subset(data,
+                      DateTime >= "2007-02-01" &
+                      DateTime < "2007-02-03")
+
+png("plot4.png", width = 480, height = 480)
+
+par(mfrow = c(2,2))
+
+plot(subset_data$DateTime,
+     subset_data$Global_active_power,
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power")
+
+plot(subset_data$DateTime,
+     subset_data$Voltage,
+     type = "l",
+     xlab = "datetime",
+     ylab = "Voltage")
+
+plot(subset_data$DateTime,
+     subset_data$Sub_metering_1,
+     type = "l",
+     xlab = "",
+     ylab = "Energy sub metering")
+
+lines(subset_data$DateTime,
+      subset_data$Sub_metering_2,
+      col = "red")
+
+lines(subset_data$DateTime,
+      subset_data$Sub_metering_3,
+      col = "blue")
+
+legend("topright",
+       legend = c("Sub_metering_1",
+                  "Sub_metering_2",
+                  "Sub_metering_3"),
+       col = c("black", "red", "blue"),
+       lty = 1,
+       cex = 0.8)
+
+plot(subset_data$DateTime,
+     subset_data$Global_reactive_power,
+     type = "l",
+     xlab = "datetime",
+     ylab = "Global Reactive Power")
+
+dev.off()
